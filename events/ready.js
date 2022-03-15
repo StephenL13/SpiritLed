36f3064ac852a799c5498bb5571a5944618dabc7
Stephen L <luciajastephen@gmail.com>
2022-03-16 00:21:13 +0800

    Big brain repository reformation

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,8 @@
 const client = require('../index.js').client
+const { createCmd } = require('../dataHandler')
 
 client.on('ready', ready => {
   console.log('The bot is ready.')
   client.user.setActivity('John 3:16-17')
+  createCmd(client)
 })
